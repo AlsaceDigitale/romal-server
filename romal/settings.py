af816095f2b69be56ba272df5e32896fa5ec4aae
Yannick Jost <yannick@yjost.com>
2018-04-06 23:26:34 +0200

    Use configured database in scalingo

--- romal/settings.py
+++ romal/settings.py
@@ -80,12 +80,17 @@ WSGI_APPLICATION = 'romal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': env.db(),  # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
