6d64dd17b096724b9fad536a62451fd6fa2ba3c2
Jonathan Hurter <john@scalingo.com>
2018-04-20 21:33:29 +0200

    Link the bot to the challenges API

--- challenges/management/commands/run_bot.py
+++ challenges/management/commands/run_bot.py
@@ -1,38 +1,55 @@
-from django.core.management.base import BaseCommand, CommandError
-
 import os
 import telebot
 import requests
+
+
+from django.core.management.base import BaseCommand, CommandError
+from clarifai.rest import ClarifaiApp
+
+from challenges.models import Challenge
 
 bot_api_token = os.environ['BOT_API_TOKEN']
 bot = telebot.TeleBot(bot_api_token)
 
 download_root = "https://api.telegram.org/file/bot{token}/".format(token=bot_api_token)
 
+current_challenge = None
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, u"Hello, welcome to this bot!")
 
-
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, "I think too that " + message.text)
-
+@bot.message_handler(commands=['new'])
+def new_challenge(message):
+    global current_challenge
+    current_challenge = Challenge.objects.order_by('?').first()
+    bot.reply_to(message, current_challenge.riddle_text)
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
+    global current_challenge
     bot.reply_to(message, "Cool photo !")
+    if current_challenge is None:
+        bot.reply_to(message, "No challenge running. Run the /new to start a new challenge")
+        return
+
     ph = message.photo[-1]
     fil = bot.get_file(ph.file_id)
     dl_url = download_root + fil.file_path
 
-    photo_data_resp = requests.get(dl_url)
+    response = current_challenge.solve(dl_url)
+    if response['valid']:
+        bot.reply_to(message, "GG WP!")
+        current_challenge = Challenge.objects.order_by('?').first()
+        bot.reply_to(message, current_challenge.riddle_text)
+        return
 
-    photo_data_resp.raise_for_status()
+    result_message = "That's not what I asked, i only see "
 
-    open(os.path.basename(fil.file_path),"wb").write(photo_data_resp.content)
+    for prediction in response['guessed']['concepts']:
+        result_message += prediction['name'] + ", "
 
+    bot.reply_to(message, result_message)
 
 
 
